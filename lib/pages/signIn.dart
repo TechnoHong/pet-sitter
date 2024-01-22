1eaf93ae8416124348b8f2882f71cb235ff2ccb0
Seokjun Hong <tjrwns51@gmail.com>
2024-01-22 10:57:35 +0900

    [gui] 로그인 화면 gui 수정

--- lib/pages/signIn.dart
+++ lib/pages/signIn.dart
@@ -4,8 +4,6 @@ import 'package:get/get.dart';
 import 'package:petke/controllers/sign_in_controller.dart';
 import 'package:petke/routes.dart';
 
-import '../layout/petkeAppBar.dart';
-
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
 
@@ -15,45 +13,62 @@ class SignInPage extends StatelessWidget {
     final controller = Get.put(SignInController());
 
     return Scaffold(
-      appBar: PetkeAppBar(
-        title: localizations.sign_in_title,
-        hasBack: true,
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            TextField(
-              controller: controller.email,
-              decoration: InputDecoration(
-                hintText: localizations.email,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
+          alignment: Alignment.center,
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: controller.email,
+                    decoration: InputDecoration(
+                      hintText: localizations.email,
+                      contentPadding: const EdgeInsets.all(8),
+                      constraints: const BoxConstraints(
+                        maxWidth: 600,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    controller: controller.password,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: localizations.password,
+                      contentPadding: const EdgeInsets.all(8),
+                      constraints: const BoxConstraints(
+                        maxWidth: 600,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  FilledButton(
+                    onPressed: () {
+                      SignInController.instance.signIn();
+                    },
+                    child: Text(localizations.sign_in_title),
+                  ),
+                  const SizedBox(height: 5),
+                  TextButton(
+                    onPressed: () {
+                      Get.toNamed(Routes.signUpScreen);
+                    },
+                    child: Text(localizations.sign_up_with_email),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      SignInController.instance.signInWithGoogle();
+                    },
+                    child: Text(localizations.sign_up_with_google),
+                  ),
+                ],
               ),
             ),
-            TextField(
-              controller: controller.password,
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: localizations.password,
-              ),
-            ),
-            FilledButton(
-              onPressed: () {
-                SignInController.instance.signIn();
-              },
-              child: Text(localizations.sign_in_title),
-            ),
-            FilledButton(
-              onPressed: () {
-                Get.toNamed(Routes.signUpScreen);
-              },
-              child: Text(localizations.sign_up_with_email),
-            ),
-            FilledButton(
-              onPressed: () {
-                SignInController.instance.signInWithGoogle();
-              },
-              child: const Text("Login with google"),
-            ),
-          ],
+          ),
         ),
       ),
     );
