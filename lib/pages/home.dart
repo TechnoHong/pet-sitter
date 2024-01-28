ad51ea84199b1c3e546327a453ea1a28a372fc62
lcoo1 <varon12@naver.com>
2024-01-28 16:01:24 +0900

    리스트 뷰

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,28 +9,39 @@ import '../routes.dart';
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final localizations = AppLocalizations.of(context)!;
-    Get.put(NavBarController());
-
-    return Center(
-      child: Column(
-        children: [
-          TextButton(
-            onPressed: () {
-              Get.toNamed(Routes.settingsScreen);
-            },
-            child: Text(localizations.settings_title),
-          ),
-          TextButton(
-            onPressed: () {
-              AuthRepository.instance.signOut();
-            },
-            child: Text(localizations.sign_out_title),
-          ),
-        ],
+  Widget listView_Separated(){
+    return ListView.separated(
+    // padding: EdgeInsets.all(),
+      scrollDirection: Axis.vertical,
+      itemCount: 100,
+      itemBuilder:(context, i) {
+        return Container(
+          height: 30,
+          child:Text('인덱스 ${i}'),
+        );
+      },
+      separatorBuilder : (BuildContext bc, int index) => const Divider(
+        color: Colors.black,
       ),
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+      final localizations = AppLocalizations.of(context)!;
+      Get.put(NavBarController());
+    return Scaffold(
+      appBar: AppBar(
+        title : Text("홈홈홈"),
+        actions: [
+          TextButton(onPressed: () {Get.toNamed(Routes.settingsScreen);}, child: Text(localizations.settings_title)),
+          TextButton(onPressed: () {AuthRepository.instance.signOut();}, child: Text(localizations.sign_out_title)),
+        ],
+      ),
+      body : SafeArea(
+        child : listView_Separated(),
+      )
+    );
+  }
+
 }
