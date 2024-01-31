4b134f98e2d9a886abc29055961bfcd3ab040e23
lcoo1 <varon12@naver.com>
2024-01-31 22:29:16 +0900

    앱바 수정

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,34 +3,47 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get/get.dart';
 import 'package:petke/controllers/nav_bar_controller.dart';
 import 'package:petke/repositories/auth_repository.dart';
+import 'package:petke/repositories/user_repository.dart';
 
 import '../routes.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    final localizations = AppLocalizations.of(context)!;
-    Get.put(NavBarController());
-
-    return Center(
-      child: Column(
-        children: [
-          TextButton(
-            onPressed: () {
-              Get.toNamed(Routes.settingsScreen);
-            },
-            child: Text(localizations.settings_title),
-          ),
-          TextButton(
-            onPressed: () {
-              AuthRepository.instance.signOut();
-            },
-            child: Text(localizations.sign_out_title),
-          ),
-        ],
+  Widget listView_Separated(){
+    return ListView.separated(
+      // padding: EdgeInsets.all(),
+      scrollDirection: Axis.vertical,
+      itemCount: 100,
+      itemBuilder:(context, i) {
+        return Container(
+          height: 30,
+          child:Text('인덱스 ${i}'),
+        );
+      },
+      separatorBuilder : (BuildContext bc, int index) => const Divider(
+        color: Colors.black,
       ),
     );
   }
+
+  writeBtnClick(){
+    Get.toNamed(Routes.initial);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Get.put(NavBarController());
+
+    return Scaffold(
+      floatingActionButton: FloatingActionButton.small(
+        onPressed: writeBtnClick,
+        child: Icon(Icons.edit),
+      ),
+      body : SafeArea(
+        child : listView_Separated(),
+      ),
+    );
+  }
+
 }
